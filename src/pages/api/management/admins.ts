4069209ce024c8c862dd5c5fd48f4488418b8321
elvis huanca <JHuanca_21@outlook.com>
2023-02-20 17:43:51 -0500

    bug in role: user

--- src/pages/api/management/admins.ts
+++ src/pages/api/management/admins.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       try {
         const users = await prisma.user.findMany({
           where: {
-            role: 'Admin' || 'Manager'
+            role: "Admin" || "Manager"
           },
         })
         res.status(200).json(users)
