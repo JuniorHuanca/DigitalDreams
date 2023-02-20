f2b096bc36ca6eab9f55cbde298d62e188e6b786
elvis huanca <JHuanca_21@outlook.com>
2023-02-20 17:46:57 -0500

    as any

--- src/pages/api/client/customers.ts
+++ src/pages/api/client/customers.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             try {
                 const users = await prisma.user.findMany({
                     where: {
-                        role: "User"
+                        role: "User" as any
                     },
                 })
                 res.status(200).json(users)
--- src/pages/api/management/admins.ts
+++ src/pages/api/management/admins.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       try {
         const users = await prisma.user.findMany({
           where: {
-            role: "Admin" || "Manager"
+            role: "Admin" || "Manager" as any
           },
         })
         res.status(200).json(users)
