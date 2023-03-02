3b6a07fd615ad15507f679c37dfaf357543d8833
elvis huanca <JHuanca_21@outlook.com>
2023-03-01 21:35:05 -0500

    types

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,7 +20,7 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_SECRET
         } as IProvider)
     ],
-    secret: process.env.NEXT_PUBLIC_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     session: {
         strategy: 'jwt',
     },
