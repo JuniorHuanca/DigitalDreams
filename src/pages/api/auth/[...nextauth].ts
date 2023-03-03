917cc211093799626e5f4aea634d22f1ce8fbdb9
elvis huanca <JHuanca_21@outlook.com>
2023-03-02 21:37:59 -0500

    other try

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "@/lib/prismadb"
 
 export const authOptions: NextAuthOptions = {
-    // adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
@@ -27,24 +27,20 @@ export const authOptions: NextAuthOptions = {
             }
             return session
         },
-        // async jwt(params) {
-        //     const { role }: any = await prisma.user.findUnique({
-        //         where: {
-        //             email: params.token.email as string,
-        //         },
-        //         select: {
-        //             role: true,
-        //         },
-        //     })
-        //     params.token.role = role
-        //     // if (params.isNewUser === true) {
-        //     //     emailProvider(params.token.email, params.token.email)
-        //     // }
-        //     return params.token
-        // },
-        async jwt({ token }) {
-            token.role = "Admin"
-            return token
+        async jwt(params) {
+            const { role }: any = await prisma.user.findUnique({
+                where: {
+                    email: params.token.email as string,
+                },
+                select: {
+                    role: true,
+                },
+            })
+            params.token.role = role
+            // if (params.isNewUser === true) {
+            //     emailProvider(params.token.email, params.token.email)
+            // }
+            return params.token
         },
     },
     // pages: {
