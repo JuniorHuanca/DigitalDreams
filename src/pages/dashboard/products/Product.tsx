f3c7b186a3c2762e1093c43c24b73dbaeb9a75d6
elvis huanca <JHuanca_21@outlook.com>
2023-03-03 21:32:00 -0500

    without product component

--- src/pages/dashboard/products/Product.tsx
+++ src/pages/dashboard/products/Product.tsx
@@ -16,63 +16,64 @@ const Product = ({ _id, name, description, price, rating, category, supply, stat
     const theme: ITheme = useTheme();
     const [isExpanded, setIsExpanded] = useState(false);
     return (
-        <Card
-            sx={{
-                backgroundImage: "none",
-                backgroundColor: theme.palette.background.alt,
-                borderRadius: "0.55rem",
-            }}
-        >
-            <CardContent>
-                <Typography
-                    sx={{ fontSize: 14 }}
-                    color={theme.palette.secondary[700]}
-                    gutterBottom
-                >
-                    {category}
-                </Typography>
-                <Typography variant="h5" component="div">
-                    {name}
-                </Typography>
-                <Typography sx={{ mb: "1.5rem" }} color={theme.palette.secondary[400]}>
-                    ${Number(price).toFixed(2)}
-                </Typography>
-                <Rating value={rating} readOnly />
+        // <Card
+        //     sx={{
+        //         backgroundImage: "none",
+        //         backgroundColor: theme.palette.background.alt,
+        //         borderRadius: "0.55rem",
+        //     }}
+        // >
+        //     <CardContent>
+        //         <Typography
+        //             sx={{ fontSize: 14 }}
+        //             color={theme.palette.secondary[700]}
+        //             gutterBottom
+        //         >
+        //             {category}
+        //         </Typography>
+        //         <Typography variant="h5" component="div">
+        //             {name}
+        //         </Typography>
+        //         <Typography sx={{ mb: "1.5rem" }} color={theme.palette.secondary[400]}>
+        //             ${Number(price).toFixed(2)}
+        //         </Typography>
+        //         <Rating value={rating} readOnly />
 
-                <Typography variant="body2">{description}</Typography>
-            </CardContent>
-            <CardActions>
-                <Button
-                    // variant="primary"
-                    sx={{
-                        variant: "primary"
-                    }}
-                    size="small"
-                    onClick={() => setIsExpanded(!isExpanded)}
-                >
-                    See More
-                </Button>
-            </CardActions>
-            <Collapse
-                in={isExpanded}
-                timeout="auto"
-                unmountOnExit
-                sx={{
-                    color: theme.palette.neutral[300],
-                }}
-            >
-                <CardContent>
-                    <Typography>id: {_id}</Typography>
-                    <Typography>Supply Left: {supply}</Typography>
-                    <Typography>
-                        Yearly Sales This Year: {stat.yearlySalesTotal}
-                    </Typography>
-                    <Typography>
-                        Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}
-                    </Typography>
-                </CardContent>
-            </Collapse>
-        </Card>
+        //         <Typography variant="body2">{description}</Typography>
+        //     </CardContent>
+        //     <CardActions>
+        //         <Button
+        //             // variant="primary"
+        //             sx={{
+        //                 variant: "primary"
+        //             }}
+        //             size="small"
+        //             onClick={() => setIsExpanded(!isExpanded)}
+        //         >
+        //             See More
+        //         </Button>
+        //     </CardActions>
+        //     <Collapse
+        //         in={isExpanded}
+        //         timeout="auto"
+        //         unmountOnExit
+        //         sx={{
+        //             color: theme.palette.neutral[300],
+        //         }}
+        //     >
+        //         <CardContent>
+        //             <Typography>id: {_id}</Typography>
+        //             <Typography>Supply Left: {supply}</Typography>
+        //             <Typography>
+        //                 Yearly Sales This Year: {stat.yearlySalesTotal}
+        //             </Typography>
+        //             <Typography>
+        //                 Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}
+        //             </Typography>
+        //         </CardContent>
+        //     </Collapse>
+        // </Card>
+        <h1>Product</h1>
     )
 }
 export default Product
