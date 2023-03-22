d98238ed124611a456811ab3ef9ff12509cdafd1
elvis huanca <JHuanca_21@outlook.com>
2023-03-22 16:22:33 -0500

    fix profile

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -5,7 +5,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { method } = req
     const { email } = req.query
     const { user } = req.body
-    console.log(user)
+    // console.log(user)
     switch (method) {
         case 'GET':
             try {
