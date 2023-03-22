7c41f6be3d1ab80eb032feb4570bdc13848aed33
elvis huanca <JHuanca_21@outlook.com>
2023-03-22 16:33:54 -0500

    without layout

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -41,80 +41,80 @@ const Settings = (props: Props) => {
     })()
   }, [userStatus, user])
   return (
-    <Layout>
+    // <Layout>
+    //   <div>
+    <LayoutProfile>
       <div>
-        <LayoutProfile>
-          <div>
-            <div className="flex flex-col items-center">
-              <div className="flex flex-col justify-center w-full p-8 bg-slate-100 dark:bg-primary-500 rounded-lg">
-                <h1 className="text-4xl font-bold mb-4">My profile</h1>
-                <div className="flex items-center gap-4">
-                  {user?.image && !imageError ? <Image
-                    className="rounded-full"
-                    src={user?.image}
-                    alt="user"
-                    width={"200px"}
-                    height={"200px"}
-                    onError={() => setImageError(true)}
-                  /> : user?.image && <Avatar name={user?.name} size="200" round={true} />}
-                  {!user?.image && <Avatar name={user?.name} size="200" round={true} />}
-                  <Image src={user?.image}></Image>
-                  <div>
-                    <p>Personaliza tu cuenta con una foto. La foto de perfil aparecerá en las aplicaciones y dispositivos que usan tu cuenta de Microsoft.</p>
-                    <div className='flex'>
-                      <input
-                        ref={ref}
-                        type="file"
-                        className='hidden'
-                      />
-                      <button className='text-xl font-semibold border border-slate-200 dark:border-primary-400 hover:bg-gray-300 hover:bg-opacity-50 rounded-lg px-6 py-4' type="button" onClick={() => ref.current?.click()}>
-                        Update image
-                      </button>
-                    </div>
-                  </div>
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col justify-center w-full p-8 bg-slate-100 dark:bg-primary-500 rounded-lg">
+            <h1 className="text-4xl font-bold mb-4">My profile</h1>
+            <div className="flex items-center gap-4">
+              {user?.image && !imageError ? <Image
+                className="rounded-full"
+                src={user?.image}
+                alt="user"
+                width={"200px"}
+                height={"200px"}
+                onError={() => setImageError(true)}
+              /> : user?.image && <Avatar name={user?.name} size="200" round={true} />}
+              {!user?.image && <Avatar name={user?.name} size="200" round={true} />}
+              <Image src={user?.image}></Image>
+              <div>
+                <p>Personaliza tu cuenta con una foto. La foto de perfil aparecerá en las aplicaciones y dispositivos que usan tu cuenta de Microsoft.</p>
+                <div className='flex'>
+                  <input
+                    ref={ref}
+                    type="file"
+                    className='hidden'
+                  />
+                  <button className='text-xl font-semibold border border-slate-200 dark:border-primary-400 hover:bg-gray-300 hover:bg-opacity-50 rounded-lg px-6 py-4' type="button" onClick={() => ref.current?.click()}>
+                    Update image
+                  </button>
                 </div>
-                <div className="w-full border-[1px] border-slate-200 dark:border-primary-400 my-4"></div>
-                <div className="flex flex-col gap-4 w-full">
-                  <h2 className="flex  gap-4 text-2xl font-bold">Account Information <button onClick={() => setSeletUser(user)}><FaPencilAlt /></button></h2>
-                  <div className="flex gap-4">
-                    <h3 className="w-2/4">Name:</h3>
-                    <div className="border-2 border-slate-200 dark:border-primary-400 py-2 px-4">
-                      {user?.name}
-                    </div>
-                  </div>
-                  <div className="flex gap-4">
-                    <h3 className="w-2/4">Username:</h3>
-                    <div className="border-2 border-slate-200 dark:border-primary-400 py-2 px-4">
-                      {user?.username}
-                    </div>
-                  </div>
-                  <div className="flex gap-4">
-                    <h3 className="w-2/4">Email:</h3>
-                    <div className="border-2 border-slate-200 dark:border-primary-400 py-2 px-4">
-                      {user?.email}
-                    </div>
-                  </div>
-                </div>
-                {/* <div className="flex flex-col gap-4 w-full">
-              <h2 className="text-2xl font-bold">Account Password</h2>
-              <div className="flex gap-4">
-                <h3 className="w-2/4">Password:</h3>
-                <div className="border-2 border-black py-2 px-4">
-                  {user.password}
-                </div>
-              </div>
-            </div> */}
               </div>
             </div>
-            {seletUser && <Profile user={seletUser} setSeletUser={setSeletUser} />}
+            <div className="w-full border-[1px] border-slate-200 dark:border-primary-400 my-4"></div>
+            <div className="flex flex-col gap-4 w-full">
+              <h2 className="flex  gap-4 text-2xl font-bold">Account Information <button onClick={() => setSeletUser(user)}><FaPencilAlt /></button></h2>
+              <div className="flex gap-4">
+                <h3 className="w-2/4">Name:</h3>
+                <div className="border-2 border-slate-200 dark:border-primary-400 py-2 px-4">
+                  {user?.name}
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="w-2/4">Username:</h3>
+                <div className="border-2 border-slate-200 dark:border-primary-400 py-2 px-4">
+                  {user?.username}
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="w-2/4">Email:</h3>
+                <div className="border-2 border-slate-200 dark:border-primary-400 py-2 px-4">
+                  {user?.email}
+                </div>
+              </div>
+            </div>
+            {/* <div className="flex flex-col gap-4 w-full">
+        <h2 className="text-2xl font-bold">Account Password</h2>
+        <div className="flex gap-4">
+          <h3 className="w-2/4">Password:</h3>
+          <div className="border-2 border-black py-2 px-4">
+            {user.password}
           </div>
-          <Toaster
-            position="top-left"
-            reverseOrder={true}
-          />
-        </LayoutProfile>
+        </div>
+      </div> */}
+          </div>
+        </div>
+        {seletUser && <Profile user={seletUser} setSeletUser={setSeletUser} />}
       </div>
-    </Layout >
+      <Toaster
+        position="top-left"
+        reverseOrder={true}
+      />
+    </LayoutProfile>
+    //   </div>
+    // </Layout>
   )
 }
 
