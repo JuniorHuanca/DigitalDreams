57cecb77ee6621ed165e1e4e1a25d8c3e3f720c2
elvis huanca <JHuanca_21@outlook.com>
2023-03-22 17:31:00 -0500

    page home

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layouts/Layout"
+
+type Props = {}
+
+const home = (props: Props) => {
+    return (
+        <Layout>
+            <div>home</div>
+            <button name="btn" onClick={(e) => console.log(e.target)}>target</button>
+        </Layout>
+    )
+}
+
+export default home
