70ab74adddd77f057b039999dd083230e3fa4cf6
elvis huanca <JHuanca_21@outlook.com>
2023-04-03 15:05:44 -0500

    data and route for first insert

---
+++ src/pages/api/data.ts
@@ -0,0 +1,92 @@
+import prisma from '@/lib/prismadb';
+import { NextApiRequest, NextApiResponse } from "next"
+import brandsData from '@/shared/util/data/brands'
+import categoriesData from '@/shared/util/data/categories'
+import countriesData from '@/shared/util/data/countries'
+import productsData from '@/shared/util/data/products'
+import subcategoriesData from '@/shared/util/data/subcategories'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req
+    const { brandsQ, categoriesQ, countriesQ, subcategoriesQ, productsQ, } = req.query
+    switch (method) {
+        case 'GET':
+            try {
+                if (brandsQ) {
+                    const brands = await prisma.brand.findMany()
+                    return res.status(200).json(brands)
+                }
+                if (categoriesQ) {
+                    const categories = await prisma.category.findMany()
+                    return res.status(200).json(categories)
+                }
+                if (countriesQ) {
+                    const countries = await prisma.country.findMany()
+                    return res.status(200).json(countries)
+                }
+                if (subcategoriesQ) {
+                    const subcategories = await prisma.subcategory.findMany()
+                    return res.status(200).json(subcategories)
+                }
+                if (productsQ) {
+                    const products = await prisma.product.findMany({
+                        include: {
+                            brand: true,
+                            subcategory: {
+                                include: {
+                                    category: true
+                                }
+                            }
+                        },
+                    })
+
+                    return res.status(200).json(products)
+                }
+                const brands = await prisma.brand.findMany()
+                const categories = await prisma.category.findMany()
+                const countries = await prisma.country.findMany()
+                const subcategories = await prisma.subcategory.findMany()
+                const products = await prisma.product.findMany()
+                return res.status(200).json({
+                    success: true,
+                    brands: brands.length,
+                    categories: categories.length,
+                    countries: countries.length,
+                    subcategories: subcategories.length,
+                    products: products.length,
+                })
+            } catch (error) {
+                return res.status(400).json({ success: false })
+            }
+        case 'POST':
+            try {
+                const brands = await prisma.brand.createMany({
+                    data: brandsData
+                })
+                const categories = await prisma.category.createMany({
+                    data: categoriesData
+                })
+                const countries = await prisma.country.createMany({
+                    data: countriesData
+                })
+                const subcategories = await prisma.subcategory.createMany({
+                    data: subcategoriesData
+                })
+                const products = await prisma.product.createMany({
+                    data: productsData
+                })
+                return res.status(200).json({
+                    brands,
+                    categories,
+                    countries,
+                    subcategories,
+                    products,
+                })
+            } catch (error) {
+                return res.status(400).json({ success: false })
+            }
+        default:
+            res.status(400).json({ success: false })
+            break
+    }
+}
---
+++ src/prisma/migrations/20230403195526_initial_models/migration.sql
@@ -0,0 +1,294 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('User', 'Admin', 'Manager');
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "username" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "password" TEXT,
+    "city" TEXT,
+    "state" TEXT,
+    "country" TEXT,
+    "occupation" TEXT,
+    "phoneNumber" TEXT,
+    "provider" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'User',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT,
+    "cost" TEXT NOT NULL,
+    "affiliateStat_id" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "brand_id" INTEGER NOT NULL,
+    "subcategory_id" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "description" TEXT NOT NULL,
+    "rating" DOUBLE PRECISION DEFAULT 0,
+    "stock" INTEGER NOT NULL DEFAULT 25,
+    "soldCount" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Brand" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Brand_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AffiliateStat" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AffiliateStat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OverallStat" (
+    "id" SERIAL NOT NULL,
+    "totalCustomers" INTEGER NOT NULL,
+    "yearlySalesTotal" DOUBLE PRECISION NOT NULL,
+    "yearlyTotalSoldUnits" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "salesByCategory" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OverallStat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductStat" (
+    "id" SERIAL NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "yearlySalesTotal" DOUBLE PRECISION NOT NULL,
+    "yearlyTotalSoldUnits" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductStat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MonthlyData" (
+    "id" SERIAL NOT NULL,
+    "month" TEXT NOT NULL,
+    "totalSales" DOUBLE PRECISION NOT NULL,
+    "totalUnits" INTEGER NOT NULL,
+    "productStat_id" INTEGER NOT NULL,
+    "overallStatId" INTEGER,
+
+    CONSTRAINT "MonthlyData_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DailyData" (
+    "id" SERIAL NOT NULL,
+    "date" TEXT NOT NULL,
+    "totalSales" DOUBLE PRECISION NOT NULL,
+    "totalUnits" INTEGER NOT NULL,
+    "productStat_id" INTEGER NOT NULL,
+    "overallStatId" INTEGER,
+
+    CONSTRAINT "DailyData_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subcategory" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "category_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Subcategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Country" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "flag" TEXT NOT NULL,
+
+    CONSTRAINT "Country_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" SERIAL NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Favorite" (
+    "id" SERIAL NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "product_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Favorite_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ProductToTransaction" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Favorite_user_id_product_id_key" ON "Favorite"("user_id", "product_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ProductToTransaction_AB_unique" ON "_ProductToTransaction"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ProductToTransaction_B_index" ON "_ProductToTransaction"("B");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_affiliateStat_id_fkey" FOREIGN KEY ("affiliateStat_id") REFERENCES "AffiliateStat"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_brand_id_fkey" FOREIGN KEY ("brand_id") REFERENCES "Brand"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_subcategory_id_fkey" FOREIGN KEY ("subcategory_id") REFERENCES "Subcategory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AffiliateStat" ADD CONSTRAINT "AffiliateStat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductStat" ADD CONSTRAINT "ProductStat_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MonthlyData" ADD CONSTRAINT "MonthlyData_productStat_id_fkey" FOREIGN KEY ("productStat_id") REFERENCES "ProductStat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MonthlyData" ADD CONSTRAINT "MonthlyData_overallStatId_fkey" FOREIGN KEY ("overallStatId") REFERENCES "OverallStat"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DailyData" ADD CONSTRAINT "DailyData_productStat_id_fkey" FOREIGN KEY ("productStat_id") REFERENCES "ProductStat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DailyData" ADD CONSTRAINT "DailyData_overallStatId_fkey" FOREIGN KEY ("overallStatId") REFERENCES "OverallStat"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subcategory" ADD CONSTRAINT "Subcategory_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favorite" ADD CONSTRAINT "Favorite_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favorite" ADD CONSTRAINT "Favorite_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProductToTransaction" ADD CONSTRAINT "_ProductToTransaction_A_fkey" FOREIGN KEY ("A") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProductToTransaction" ADD CONSTRAINT "_ProductToTransaction_B_fkey" FOREIGN KEY ("B") REFERENCES "Transaction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
