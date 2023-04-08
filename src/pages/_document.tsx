a8b7d32d13289cd78ba785d588df7174a94433c3
elvis huanca <JHuanca_21@outlook.com>
2023-04-08 10:15:22 -0500

    preload font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head >
         <meta name="description" content="Encuentra los mejores productos tecnológicos en nuestro e-commerce: computadoras, ratones, teclados, y luces LED. Ofrecemos una amplia variedad de marcas y modelos, con características que se adaptan a las necesidades de cada usuario. Además, contamos con precios competitivos y un servicio de atención al cliente excepcional. ¡Visítanos y encuentra todo lo que necesitas para estar a la vanguardia de la tecnología!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preload" href="https://fonts.googleapis.com/css?family=Montserrat:400,800&display=swap" as="font" />
         <link rel="icon" href="/github.png" />
       </Head>
       <body>
