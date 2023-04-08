a333d2e3795090009684c835ab43c33520582ec4
elvis huanca <JHuanca_21@outlook.com>
2023-04-08 10:00:03 -0500

    changes for CEO

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -21,6 +21,8 @@ const Layout = ({ children, tittle }: Props) => {
     return (
         <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
             <Head>
+                <meta name="description" content="Encuentra los mejores productos tecnológicos en nuestro e-commerce: computadoras, ratones, teclados, y luces LED. Ofrecemos una amplia variedad de marcas y modelos, con características que se adaptan a las necesidades de cada usuario. Además, contamos con precios competitivos y un servicio de atención al cliente excepcional. ¡Visítanos y encuentra todo lo que necesitas para estar a la vanguardia de la tecnología!" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>{tittle}</title>
                 <link rel="icon" href="/github.png" />
             </Head>
