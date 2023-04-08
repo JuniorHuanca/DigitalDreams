790ce4920ad7a05e78378695460f67349db1f004
elvis huanca <JHuanca_21@outlook.com>
2023-04-08 10:08:05 -0500

    more changes for optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="description" content="Encuentra los mejores productos tecnológicos en nuestro e-commerce: computadoras, ratones, teclados, y luces LED. Ofrecemos una amplia variedad de marcas y modelos, con características que se adaptan a las necesidades de cada usuario. Además, contamos con precios competitivos y un servicio de atención al cliente excepcional. ¡Visítanos y encuentra todo lo que necesitas para estar a la vanguardia de la tecnología!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/github.png" />
+      </Head>
       <body>
         <div className="app">
-        <Main />
-        <NextScript />
+          <Main />
+          <NextScript />
         </div>
       </body>
     </Html>
