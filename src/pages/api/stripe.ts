45a981a27566206fe46b7ab7beeb7fc9b320aca2
elvis huanca <JHuanca_21@outlook.com>
2023-04-24 21:07:25 -0500

    stripe

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -1,57 +1,59 @@
 import Stripe from "stripe";
 import { NextApiRequest, NextApiResponse } from "next";
+import { IProductCart } from "@/shared/util/types";
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string, {
-  apiVersion: "2022-11-15",
-});
+// const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string, {
+//   apiVersion: "2022-11-15",
+// });
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    // try {
-    //   const params = {
-    //     submit_type: "pay",
-    //     mode: "payment",
-    //     payment_method_types: ["card"],
-    //     billing_address_collection: "auto",
-    //     shipping_options: [{ shipping_rate: "shr_1Kn3IaEnylLNWUqj5rqhg9oV" }],
-    //     line_items: req.body.map((item: any) => {
-    //       const img = item.image[0].asset._ref;
-    //       const newImage = img
-    //         .replace(
-    //           "image-",
-    //           "https://cdn.sanity.io/images/vfxfwnaw/production/"
-    //         )
-    //         .replace("-webp", ".webp");
+    try {
+      // Create Checkout Sessions from body params.
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        // shipping_options: [{ shipping_rate: "shr_1Kn3IaEnylLNWUqj5rqhg9oV" }],
+        line_items: req.body.map((item: IProductCart) => {
+          // const img = item.image[0].asset._ref;
+          // const newImage = img
+          //   .replace(
+          //     "image-",
+          //     "https://cdn.sanity.io/images/vfxfwnaw/production/"
+          //   )
+          //   .replace("-webp", ".webp");
 
-    //       return {
-    //         price_data: {
-    //           currency: "usd",
-    //           product_data: {
-    //             name: item.name,
-    //             images: [newImage],
-    //           },
-    //           unit_amount: item.price * 100,
-    //         },
-    //         adjustable_quantity: {
-    //           enabled: true,
-    //           minimum: 1,
-    //         },
-    //         quantity: item.quantity,
-    //       };
-    //     }),
-    //     success_url: `${req.headers.origin}/success`,
-    //     cancel_url: `${req.headers.origin}/canceled`,
-    //   };
-
-    //   // Create Checkout Sessions from body params.
-    //   const session = await stripe.checkout.sessions.create(params);
-
-    //   res.status(200).json(session);
-    // } catch (err: any) {
-    //   res.status(err.statusCode || 500).json(err.message);
-    // }
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.product.name,
+                images: item.product.image,
+              },
+              unit_amount: item.product.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      };
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err: any) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
