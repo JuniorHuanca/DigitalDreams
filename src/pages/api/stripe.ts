9bb10c65e33c75ee000fc938123f360a5a31bd24
elvis huanca <JHuanca_21@outlook.com>
2023-04-26 17:59:17 -0500

    shipping_rate_data

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -143,8 +143,30 @@ export default async function handler(
           ],
         },
         shipping_options: [
-          { shipping_rate: "shr_1N1G6LK9GNzBJvollyJ8qvQp" },
-          { shipping_rate: "shr_1N1G8cK9GNzBJvolaO57Ea3t" },
+          { shipping_rate: "shr_1N1HF5K9GNzBJvolaqFlPJ0H" },
+          { shipping_rate: "shr_1N1HGJK9GNzBJvol0uXFKlnp" },
+          // {
+          //   shipping_rate_data: {
+          //     type: 'fixed_amount',
+          //     fixed_amount: {amount: 0, currency: 'usd'},
+          //     display_name: 'Free shipping',
+          //     delivery_estimate: {
+          //       minimum: {unit: 'business_day', value: 5},
+          //       maximum: {unit: 'business_day', value: 7},
+          //     },
+          //   },
+          // },
+          // {
+          //   shipping_rate_data: {
+          //     type: 'fixed_amount',
+          //     fixed_amount: {amount: 1500, currency: 'usd'},
+          //     display_name: 'Next day air',
+          //     delivery_estimate: {
+          //       minimum: {unit: 'business_day', value: 1},
+          //       maximum: {unit: 'business_day', value: 1},
+          //     },
+          //   },
+          // },
         ],
         line_items: req.body.map((item: IProductCart) => {
           return {
