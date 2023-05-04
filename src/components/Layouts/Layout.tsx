8d802d79b4b2f5ee8cee68be3cb241fb7f17b547
elvis huanca <JHuanca_21@outlook.com>
2023-05-04 11:45:42 -0500

    login mounted fix

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,39 +1,49 @@
-import { Box, useMediaQuery } from '@mui/material';
-import Navbar from '../Navbar/Navbar';
-import { useSession } from 'next-auth/react'
-import Loader from '../Loaders/Loader';
-import Head from 'next/head';
-import Footer from '../Footer/Footer';
+import { Box, useMediaQuery } from "@mui/material";
+import Navbar from "../Navbar/Navbar";
+import { useSession } from "next-auth/react";
+import Loader from "../Loaders/Loader";
+import Head from "next/head";
+import Footer from "../Footer/Footer";
+import { useEffect, useState } from "react";
 interface ISession {
-    data: any;
-    status: string;
+  data: any;
+  status: string;
 }
 interface Props {
-    children: React.ReactNode;
-    title: string | undefined
+  children: React.ReactNode;
+  title: string | undefined;
 }
 const Layout = ({ children, title }: Props) => {
-    const { data: session, status }: ISession = useSession()
-    const isNonMobile = useMediaQuery("(min-width: 600px)");
-    if (status === "loading") {
-        <Loader />
-    }
-    return (
-        <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
-            <Head>
-                <title>{title}</title>
-            </Head>
-            <Box flexGrow={1}>
-                <Navbar
-                    user={session?.user}
-                // isSidebarOpen={isSidebarOpen}
-                // setIsSidebarOpen={setIsSidebarOpen}
-                />
-                {children}
-                <Footer />
-            </Box>
-        </Box>
-    )
-}
+  const { data: session, status }: ISession = useSession();
+  const [mounted, setMounted] = useState<boolean>(false);
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
 
-export default Layout
+  useEffect(() => {
+    setMounted(true);
+    return () => {
+      setMounted(false);
+    };
+  }, [status, session]);
+
+  if (status === "loading" || !mounted) {
+    return <Loader />;
+  }
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Box flexGrow={1}>
+        <Navbar
+          user={session?.user}
+          // isSidebarOpen={isSidebarOpen}
+          // setIsSidebarOpen={setIsSidebarOpen}
+        />
+        {children}
+        <Footer />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
