0fa0d17dada7f9308dc19dc9e44e279790815125
elvis huanca <JHuanca_21@outlook.com>
2023-05-14 11:52:18 -0500

    nuevo nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,4 @@
-import {
-  Search,
-  Clear,
-} from "@mui/icons-material";
+import { Search, Clear } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import FlexBetween from "../FlexBetween";
 import {
@@ -489,6 +486,39 @@ const Navbar = ({ user }: Props) => {
       )}
     </AppBar>
   );
+  // return (
+  //   <div className="nav">
+  //     <input type="checkbox" id="nav-check" />
+  //     <div className="nav-header">
+  //       <div className="nav-title">JoGeek</div>
+  //     </div>
+  //     <div className="nav-btn">
+  //       <label htmlFor="nav-check">
+  //         <span></span>
+  //         <span></span>
+  //         <span></span>
+  //       </label>
+  //     </div>
+
+  //     <div className="nav-links">
+  //       <a href="//github.io/jo_geek" target="_blank">
+  //         Github
+  //       </a>
+  //       <a href="http://stackoverflow.com/users/4084003/" target="_blank">
+  //         Stackoverflow
+  //       </a>
+  //       <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">
+  //         LinkedIn
+  //       </a>
+  //       <a href="https://codepen.io/jo_Geek/" target="_blank">
+  //         Codepen
+  //       </a>
+  //       <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">
+  //         JsFiddle
+  //       </a>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Navbar;
